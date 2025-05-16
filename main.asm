f4e836e1dc5ca70ec75f5cb6e679c4e9ddf9bd88
adii <77727680+AdityaMotale@users.noreply.github.com>
2025-05-17 02:11:52 +0530

    parse loop to read file names

--- main.asm
+++ main.asm
@@ -5,6 +5,7 @@ global _start
 section .bss
   cwd_buf resb 4096
   dent_buf resb 8192
+  file resb 4096
 
 section .text
 _start:
@@ -27,7 +28,7 @@ _start:
   test rax, rax
   js error_exit
 
-  mov r12, rax
+  mov r12, rax                  ; fd of opened cwd
 
   ;; getdents64 syscall
   mov rax, 217
@@ -36,13 +37,52 @@ _start:
   mov rdx, 8192
   syscall
 
-  ;; print the dent buf
-  mov rdx, rax                  ; len of the buffer
+  mov r13, rax                  ; sizeof(dent_buf)
+
+parse_loop:
+  lea rcx, [dent_buf]           ; pointer to dent_buf
+  xor rbx, rbx                  ; offset into dent_buf
+
+.init_loop:
+  cmp rbx, r13
+  jae .done                     ; close the loop
+
+  ;; entry starts at (dent_buf + rbx)
+  mov r11, rbx
+  add r11, 19                   ; offset to first char in d_name
+
+  xor rdx, rdx                  ; sizeof(file)
+  lea rsi, [file]               ; pointer to file buf
+
+.loop:
+  mov r12, rcx
+  add r12, rdx
+  add r12, r11
+
+  mov al, [r12]
+
+  cmp al, 0x00
+  je .loop_end
+
+  mov [rsi + rdx], al
+  inc rdx
+
+  jmp .loop
+
+.loop_end:
+  mov byte [rsi + rdx], 0x00
+  inc rdx
+
+.print_file:
   mov rax, 0x01
   mov rdi, 0x01
-  lea rsi, [dent_buf]
   syscall
 
+.next:
+  add rdx, r10
+  jmp .init_loop
+
+.done:
   jmp exit
 
 error_exit:
