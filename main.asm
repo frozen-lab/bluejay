21f821db5c176c5077abe8bd8e9964d78646c2fa
adii <77727680+AdityaMotale@users.noreply.github.com>
2025-05-16 15:53:58 +0530

    reading current directory path `cwd`

--- main.asm
+++ main.asm
@@ -6,20 +6,24 @@ section .rodata
   file db "shell.nix", 0x00
 
 section .bss
+  cwd_buf resb 4096
   buf resb 0x01
 
 section .text
 _start:
-  mov rax, 0x02
-  lea rdi, [file]
-  xor rsi, rsi                  ; flags = O_READONLY
-  xor rdx, rdx                  ; mode = NA
+  mov rax, 79
+  lea rdi, [cwd_buf]
+  mov rsi, 4096
   syscall
 
   test rax, rax
-  jz error_exit
+  js error_exit
 
-  mov r12, rax
+  mov rax, 0x01
+  mov rdi, 0x01
+  lea rsi, [cwd_buf]
+  mov rdx, 4096
+  syscall
 
   jmp exit
 
